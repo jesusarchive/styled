8a6a7ad59e49305bd787cb35e430c2d72b28b0a8
Jesús <jbrdev21880949243098@protonmail.com>
2021-01-24 10:39:17 +0100

    styled-minimal

---
+++ src/components/Flex.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Box, { basePropTypes } from './Box';
+
+const Flex = styled(Box)({
+    display: 'flex'
+});
+
+Flex.displayName = 'Flex';
+
+Flex.propTypes = {
+    as: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    ...basePropTypes
+};
+
+Flex.defaultProps = {
+    alignItems: 'center'
+};
+
+export default Flex;
---
+++ src/components/utils/helpers.ts
@@ -0,0 +1,168 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import deepmerge from 'deepmerge';
+import { darken, getLuminance, lighten, opacify, parseToRgb, rgba } from 'polished';
+import * as defaultTheme from './theme';
+
+/** Check if a variable is defined **/
+export const isDefined = (val: unknown): boolean => typeof val !== 'undefined';
+
+/** Check if a variable is a number **/
+export const isNumber = (val: number | string): boolean => typeof val === 'number';
+
+/** Get textual unit value */
+export const px = (val: string | number): string => (isNumber(val) ? `${val}px` : val) as string;
+
+/**
+ *
+ * @param {string|number} n
+ * @param {Array} breakpoints
+ * @returns {string}
+ */
+const createMediaQuery = (n: string | number, breakpoints: any): string => {
+    const grid = {
+        'xs-only': `@media (min-width: ${breakpoints.ix - 1}px)`,
+        ix: `@media (min-width: ${breakpoints.ix}px)`,
+        'ix-only': `@media (max-width: ${breakpoints.md - 1}px)`,
+        md: `@media (min-width: ${breakpoints.md}px)`,
+        'md-only': `@media (max-width: ${breakpoints.lg - 1}px)`,
+        lg: `@media (min-width: ${breakpoints.lg}px)`,
+        'lg-only': `@media (max-width: ${breakpoints.xl - 1}px)`,
+        xl: `@media (min-width: ${breakpoints.xl}px)`,
+        'xl-only': `@media (max-width: ${breakpoints.xxl - 1}px)`,
+        xxl: `@media (min-width: ${breakpoints.xxl}px)`
+    } as any;
+
+    if (isNumber(n)) {
+        return `@media (min-width: ${px(n)})`;
+    }
+
+    return grid[n] || `@media (min-width: ${n})`;
+};
+
+/**
+ * Merge ThemeProvides's theme with the default theme
+ * @param {Object} props
+ *
+ * @returns {Object}
+ */
+export const mergeTheme = (props: any = {}): any => {
+    const { theme = {} } = props;
+
+    return deepmerge(defaultTheme, theme, { arrayMerge: (dest, source) => source });
+};
+
+/**
+ * Get the merged theme
+ */
+export const getTheme = (props: any, path?: string): any => {
+    const theme = mergeTheme(props);
+
+    return path ? theme[path] : theme;
+};
+
+/**
+ * SC Helper to get parts of the theme.
+ *
+ * @param {string} path
+ * @param {string} [key]
+ * @returns {function(Object): *}
+ */
+export const getStyles = (path: string, key?: string): any => (props: any): any => {
+    const styles = getTheme(props, path);
+
+    return key ? styles[key] : styles;
+};
+
+/**
+ * SC Helper to get the corresponding item from the space scale
+ *
+ * @param {number|string|Array} value
+ * @param {boolean} [pure]
+ *
+ * @returns {function}
+ */
+export const spacer = (value: any, pure?: boolean): any => (props: any): any => {
+    const { space } = getTheme(props);
+    const result = space[value] || value;
+
+    return pure ? result : px(result);
+};
+
+/**
+ * SC Helper to generate responsive media queries
+ *
+ * @param {function|Object} input
+ * @param {function} [queryBuilderFn]
+ *
+ * @returns {function}
+ */
+export const responsive = (input: any, queryBuilderFn?: any): any => (props: any): string => {
+    const { breakpoints } = getTheme(props);
+    const rules = typeof input === 'function' ? input(props) : input;
+    const queryBuilder = queryBuilderFn || createMediaQuery;
+    const result = [];
+
+    for (const rule in rules) {
+        /* istanbul ignore else */
+        if ({}.hasOwnProperty.call(rules, rule)) {
+            result.push(`
+        ${queryBuilder(rule, breakpoints)} {
+          ${rules[rule]}
+        }
+      `);
+        }
+    }
+
+    return result.join('\n');
+};
+
+/**
+ * Get color from theme
+ *
+ * @param {Object} props
+ * @param {string} base
+ *
+ * @returns {string}
+ */
+export function getColor(props: any, base = 'primary'): string {
+    const { variant } = props;
+    const { colors, palette } = getTheme(props);
+
+    return palette[variant] || colors[variant] || palette[base];
+}
+
+export const getDimmerColor = (val: string): string => {
+    try {
+        const luminance = getLuminance(val);
+        let dimmer = lighten(0.4, val);
+
+        if (luminance > 0.7) {
+            dimmer = darken(0.2, val);
+        } else if (luminance > 0.2) {
+            dimmer = lighten(0.2, val);
+        } else if (luminance > 0.1) {
+            dimmer = lighten(0.3, val);
+        }
+
+        return dimmer;
+    } catch (error) {
+        return val;
+    }
+};
+
+/**
+ * Color contrast
+ *
+ * @param {string} color
+ * @returns {number}
+ */
+export function getYiq(color: string): number {
+    const r = parseToRgb(color).red;
+    const g = parseToRgb(color).green;
+    const b = parseToRgb(color).blue;
+
+    return (r * 299 + g * 587 + b * 114) / 1000;
+}
+
+export { darken, lighten, opacify, rgba };
