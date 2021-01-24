062cbf4501eb543529cc1c9ed6ac687785cce6e8
Jesús <jbrdev21880949243098@protonmail.com>
2021-01-25 00:06:19 +0100

    j

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Alert, Container } from './components';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Container>
-        <Alert>Alert</Alert>
-      </Container>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
